ad73be8d0b7f133bcf0d828e5e4c5d83da2ce89b
Xiangyu Ma <maxiangyucarter@gmail.com>
2023-10-29 17:02:47 -0600

    first commit

---
+++ src/components/skills/Backend.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+
+const Backend = () => {
+    return (
+
+        <div className="skills__content">
+            <h3 className="skills__title">Backend developer</h3>
+
+            <div className="skills__box">
+                <div className="skills__group">
+                    <div className="skills__data">
+                        <i className="bx bx-badge-check"></i>
+
+                        <div>
+                            <h3 className="skills__name">Git</h3>
+                            <span className="skills__level">Intermediate</span>
+                        </div>
+                    </div>
+
+                    <div className="skills__data">
+                        <i className="bx bx-badge-check"></i>
+
+                        <div>
+                            <h3 className="skills__name">Python</h3>
+                            <span className="skills__level">Intermediate</span>
+                        </div>
+                    </div>
+
+                    <div className="skills__data">
+                        <i className="bx bx-badge-check"></i>
+
+                        <div>
+                            <h3 className="skills__name">Java</h3>
+                            <span className="skills__level">Intermediate</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="skills__group">
+                    <div className="skills__data">
+                        <i className="bx bx-badge-check"></i>
+
+                        <div>
+                            <h3 className="skills__name">C#</h3>
+                            <span className="skills__level">Intermediate</span>
+                        </div>
+                    </div>
+
+                    <div className="skills__data">
+                        <i className="bx bx-badge-check"></i>
+
+                        <div>
+                            <h3 className="skills__name">MySql</h3>
+                            <span className="skills__level">Advanced</span>
+                        </div>
+                    </div>
+
+                    <div className="skills__data">
+                        <i className="bx bx-badge-check"></i>
+
+                        <div>
+                            <h3 className="skills__name">PostgreSql</h3>
+                            <span className="skills__level">Advanced</span>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    )
+
+
+}
+
+
+
+export default Backend
