74ef4fdb34b6ffb82961466e93105fa65a8b8d97
Xiangyu Ma <maxiangyucarter@gmail.com>
2023-10-30 17:18:51 -0600

    for gh pages deployment

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -6,22 +6,22 @@ const Info = () => {
       <div className="about__box">
         <i class="bx bx-award about__icon"></i>
 
-        <h3 className="about__title">Experience</h3>
-        <span className="about__subtitle">8 Years Working</span>
+        <h3 className="about__title"></h3>
+        <span className="about__subtitle"></span>
       </div>
 
       <div className="about__box">
         <i class="bx bx-briefcase-alt about__icon"></i>
 
-        <h3 className="about__title">Completed</h3>
-        <span className="about__subtitle">48 + Projects</span>
+        <h3 className="about__title"></h3>
+        <span className="about__subtitle"></span>
       </div>
 
       <div className="about__box">
         <i class="bx bx-support about__icon"></i>
 
-        <h3 className="about__title">Support</h3>
-        <span className="about__subtitle">Online 24/7</span>
+        <h3 className="about__title"></h3>
+        <span className="about__subtitle"></span>
       </div>
     </div>
   );
